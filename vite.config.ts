1ff67252d1c289d531a4adcde9c8a5ef65db5a4b
Joanna <pietrykajoanna@gmail.com>
2025-03-12 17:08:19 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer], 
+      plugins: [tailwindcss, autoprefixer],
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), 
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
